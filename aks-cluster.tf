f82b6d3cb9159361e8b1ff536098b66087ce96d1
Rizan-Dygisec <103989969+Rizan-Dygisec@users.noreply.github.com>
2022-05-25 09:45:00 +0530

    s
    s

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -1,12 +1,11 @@
-resource "random_pet" "prefix" {}
 
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "default" {
-  name     = "${random_pet.prefix.id}-rg"
-  location = "West US 2"
+  name     = "triton-azure-rg"
+  location = "East Asia"
 
   tags = {
     environment = "Demo"
@@ -14,15 +13,15 @@ resource "azurerm_resource_group" "default" {
 }
 
 resource "azurerm_kubernetes_cluster" "default" {
-  name                = "${random_pet.prefix.id}-aks"
+  name                = "triton-azure-aks"
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
-  dns_prefix          = "${random_pet.prefix.id}-k8s"
+  dns_prefix          = "triton-azure-k8s"
 
   default_node_pool {
     name            = "default"
     node_count      = 2
-    vm_size         = "Standard_D2_v2"
+    vm_size         = "d16s_v4"
     os_disk_size_gb = 30
   }
 
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,2 @@
-appId    = "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"
-password = "********-****-****-****-************"
+appId    = "d151f581-223a-4997-80c2-c249c533b2d5"
+password = "yUF8Q~O5Tm4Tsp9ji4huNqKVr1VAKj6RNmmLOcve"
